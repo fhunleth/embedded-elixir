748cf1a47f53b365743f6e1a5412939bd97a52e5
Frank Hunleth <fhunleth@troodon-software.com>
2017-03-27 13:10:13 -0400

    Add travis

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -11,24 +11,32 @@ if [ "$(git status -s)" ]; then
     exit 1;
 fi
 
+SHA=$(git rev-parse HEAD)
+if [ -n "$GITHUB_API_TOKEN" ]; then
+    GIT_USER_ARGS="-c user.name='travis' -c user.email='travis'"
+fi
+
 echo "Deleting old publication"
 rm -rf public
 mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add public origin/gh-pages
-git -C public checkout -B gh-pages
-
-echo "Removing existing files"
-rm -rf public/*
+echo "Creating gh-pages branch in ./public"
+git -C public init
+git -C public checkout -b gh-pages
 
 echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
 git -C public add --all
-git -C public commit -m "Publishing to gh-pages (publish.sh)"
-git -C public push
+git -C public $GIT_USER_ARGS commit -m "Publishing to gh-pages ($SHA)"
+
+echo "Pushing gh-pages branch"
+if [ -n "$GITHUB_API_TOKEN" ]; then
+    # CI deployment
+    git -C public push -f -q https://fhunleth:$GITHUB_API_TOKEN@github.com/fhunleth/embedded-elixir-gh-pages gh-pages:gh-pages &2>/dev/null
+else
+    # Manual deployment
+    git -C public push -f git@github.com:fhunleth/embedded-elixir.git gh-pages
+fi
 
