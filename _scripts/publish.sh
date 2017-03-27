847b4808378fecf7e8c7d5a183eccfdf703fc8ff
Frank Hunleth <fhunleth@troodon-software.com>
2017-03-27 14:37:44 -0400

    Add more debugging info to git push

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -34,7 +34,9 @@ git -C public $GIT_USER_ARGS commit -m "Publishing to gh-pages ($SHA)"
 echo "Pushing gh-pages branch"
 if [ -n "$GITHUB_API_TOKEN" ]; then
     # CI deployment
-    git -C public push -f -q https://fhunleth:$GITHUB_API_TOKEN@github.com/fhunleth/embedded-elixir gh-pages:gh-pages &2>/dev/null
+    git -C public push -f https://fhunleth:$GITHUB_API_TOKEN@github.com/fhunleth/embedded-elixir gh-pages:gh-pages 2>&1 | \
+        sed s/$GITHUB_API_TOKEN/HIDDEN/g
+  #  &2>/dev/null
 else
     # Manual deployment
     git -C public push -f git@github.com:fhunleth/embedded-elixir.git gh-pages
