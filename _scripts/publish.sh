aa4532e4995e54dddb3db946b0389097429370ef
Frank Hunleth <fhunleth@troodon-software.com>
2017-03-27 14:33:20 -0400

    Update push URL from travis

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -34,7 +34,7 @@ git -C public $GIT_USER_ARGS commit -m "Publishing to gh-pages ($SHA)"
 echo "Pushing gh-pages branch"
 if [ -n "$GITHUB_API_TOKEN" ]; then
     # CI deployment
-    git -C public push -f -q https://fhunleth:$GITHUB_API_TOKEN@github.com/fhunleth/embedded-elixir-gh-pages gh-pages:gh-pages &2>/dev/null
+    git -C public push -f -q https://fhunleth:$GITHUB_API_TOKEN@github.com/fhunleth/embedded-elixir gh-pages:gh-pages &2>/dev/null
 else
     # Manual deployment
     git -C public push -f git@github.com:fhunleth/embedded-elixir.git gh-pages
