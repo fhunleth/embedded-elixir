8b0f122902609b104edbadfd5a417c8167c74885
Frank Hunleth <fhunleth@troodon-software.com>
2017-03-27 11:57:31 -0400

    Update publish script

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -28,5 +28,7 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+git -C public add --all
+git -C public commit -m "Publishing to gh-pages (publish.sh)"
+git -C public push
 
